a1820398128ff3b7a0cf7ccab830643a39b5392e
Simon Tamang <gomjasimon555@gmail.com>
2023-08-14 16:44:57 +0545

    Day 1 of react with caleb

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
+import "./App.css";
+import Employee from "./components/Employee";
 function App() {
+  console.log("We are about to list the employees");
+  const showEmployees = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ? (
+        <>
+          <Employee />
+          <Employee />
+          <Employee />
+          <Employee />
+        </>
+      ) : (
+        <p>You cannot see the employees</p>
+      )}
     </div>
   );
 }
---
+++ src/components/Employee.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Employee() {
+  return (
+   <h1>Here is an Employee</h1>
+  )
+}
+
+export default Employee
