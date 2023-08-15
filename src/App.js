419fdbe2cf46691d5c83ddab9eee0ab981b8be81
Simon Tamang <gomjasimon555@gmail.com>
2023-08-15 18:17:45 +0545

    Day-2 Update functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
-import {v4 as uuidv4} from 'uuid'
+// import {v4 as uuidv4} from 'uuid'
 function App() {
   const [role, setRole] = useState("dev");
   const [employees, setEmployees] = useState([
-    {
+    { 
+      id:1,
       name: "Caleb",
       role: "Developer",
       img: "https://images.pexels.com/photos/5750775/pexels-photo-5750775.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
     },
     {
+      id:2,
       name: "Simon",
       role: "Engineer",
       img: "https://images.pexels.com/photos/4154072/pexels-photo-4154072.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     },
     {
+      id:3,
       name: "Sal",
       role: "Manager",
       img: "https://images.pexels.com/photos/6207069/pexels-photo-6207069.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     },
     {
+      id:4,
       name: "Corey",
       role: "The Devops Guy",
       img: "https://images.pexels.com/photos/6833444/pexels-photo-6833444.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     },
     {
+      id:5,
       name: "Jake",
       role: "Senior",
       img: "https://images.pexels.com/photos/5750775/pexels-photo-5750775.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
     },
   ]);
+function updateEmployee(id,newName, newRole)
+{
+const updatedEmployees = employees.map((employee)=>{
+  if(id == employee.id)
+  {
+    return {...employee, name:newName, role:newRole};
+  }
+  return employee;
+});
+setEmployees(updatedEmployees)
+}
   const showEmployees = true;
 
   return (
@@ -50,10 +66,12 @@ function App() {
             {employees.map((employee) => {
                 return(
                 <Employee 
-                key={uuidv4()}
+                key={employee.id}
+                id={employee.id}
                 name={employee.name}
                 role={employee.role}
                 img={employee.img}
+                updateEmployee ={updateEmployee}
                 />
                 );
             })}
